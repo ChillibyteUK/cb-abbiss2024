179122e8496503cce3d033923a0815f7269d2074
LamcatUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-06-17 12:49:12 +0100

    Update cb_hero.php

--- page-templates/blocks/cb_hero.php
+++ page-templates/blocks/cb_hero.php
@@ -6,11 +6,21 @@ $class = $block['className'] ?? 'mb-4';
     class="hero <?=$page?> <?=$class?>">
     <?=wp_get_attachment_image(get_field('background'), 'full', false, array('class' => 'hero__bg', 'alt' => get_the_title()))?>
     <div class="container-xl hero__inner">
+        <?php
+        if (get_field('icon') ?? null) {
+            ?>
         <img src="<?=get_field('icon')?>"
             class="hero__icon" alt="<?=get_the_title()?>">
+        <?php
+        }
+if (get_field('content') ?? null) {
+    ?>
         <div class="hero__content">
             <?=get_field('content')?>
         </div>
+        <?php
+}
+?>
     </div>
 </section>
 <?php
