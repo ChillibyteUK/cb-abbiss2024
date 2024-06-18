b310af7cec408eccdf4f30749f48464c38462b11
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-06-18 11:08:36 +0100

    Update cb_services_nav.php

--- page-templates/blocks/cb_services_nav.php
+++ page-templates/blocks/cb_services_nav.php
@@ -3,34 +3,36 @@ $class = $block['className'] ?? 'pb-5';
 $bg = get_field('grey_bg') ? 'bg-grey-500' : '';
 $style = (get_field('compact')[0] ?? null) === 'Yes' ? 'services_nav__compact' : '';
 
+$section = strpos($_SERVER['REQUEST_URI'], '/corridor/') !== false ? '/corridor' : '/for-businesses/all-services';
+
 $link = wp_make_link_relative(get_permalink());
 ?>
 <section
     class="services_nav <?=$bg?> <?=$class?> <?=$style?>">
     <div class="container-xl">
         <?php
-        $active = $link == '/for-businesses/all-services/communications/' ? 'active' : '';
+        $active = $link == $section . '/communications/' ? 'active' : '';
 ?>
         <a class="services_nav__card <?=$active?>"
-            href="/for-businesses/all-services/communications/">
+            href="<?=$section?>/communications/">
             <img src="<?=get_stylesheet_directory_uri()?>/img/icon-communications.svg"
                 alt="Communications">
             <div>Communications</div>
         </a>
         <?php
-$active = $link == '/for-businesses/all-services/people-consulting/' ? 'active' : '';
+$active = $link == $section . '/people-consulting/' ? 'active' : '';
 ?>
         <a class="services_nav__card <?=$active?>"
-            href="/for-businesses/all-services/people-consulting/">
+            href="<?=$section?>/people-consulting/">
             <img src="<?=get_stylesheet_directory_uri()?>/img/icon-people-consulting.svg"
                 alt="People Consulting">
             <div>People<br>Consulting</div>
         </a>
         <?php
-$active = $link == '/for-businesses/all-services/law-tax/' ? 'active' : '';
+$active = $link == $section . '/law-tax/' ? 'active' : '';
 ?>
         <a class="services_nav__card <?=$active?>"
-            href="/for-businesses/all-services/law-tax/">
+            href="<?=$section?>/law-tax/">
             <img src="<?=get_stylesheet_directory_uri()?>/img/icon-law.svg"
                 alt="Law & Tax">
             <div>Law<br>&amp; Tax</div>
