cb805c7654c9e0a4a58d102fd65b9438544ad654
LamcatUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-06-19 11:56:07 +0100

    Update cb_notes_brochures.php

--- page-templates/blocks/cb_notes_brochures.php
+++ page-templates/blocks/cb_notes_brochures.php
@@ -40,46 +40,54 @@ $br = new WP_Query(
             ?>
         <div class="notes_brochures__col">
             <h3>Briefing Notes</h3>
-            <a class="notes_brochures__card" href="<?=$url?>" download="<?=$filename?>">
+            <a class="notes_brochures__card" href="<?=$url?>"
+                download="<?=$filename?>">
                 <div class="overlay"></div>
-                <div class="notes_brochures__title"><?=get_the_title()?></div>
-                <div class="notes_brochures__date"><?=get_the_date()?></div>
+                <div class="notes_brochures__title">
+                    <?=get_the_title()?>
+                </div>
+                <div class="notes_brochures__date">
+                    <?=get_the_date()?>
+                </div>
             </a>
             <?php
             if ($section == 'corridor') {
-                echo '<div class="link-arrow-inline"><a href="/corridor/knowledge/insights/">View all Corridor insights</a></div>';
-            }
-            else {
-                echo '<div class="link-arrow-inline"><a href="/knowledge/insights/">View all insights</a></div>';
+                echo '<div class="link-arrow-inline"><a href="/corridor/knowledge/briefing-notes/">View all Corridor briefing notes</a></div>';
+            } else {
+                echo '<div class="link-arrow-inline"><a href="/knowledge/briefing-notes/">View all briefing notes</a></div>';
             }
             ?>
         </div>
-            <?php
+        <?php
         }
-        if ($br->have_posts()) {
-            $br->the_post();
-            $pdf = get_field('pdf_file', get_the_ID());
-            $url = $pdf['url'];
-            $filename = $pdf['filename'];
-            ?>
+if ($br->have_posts()) {
+    $br->the_post();
+    $pdf = get_field('pdf_file', get_the_ID());
+    $url = $pdf['url'];
+    $filename = $pdf['filename'];
+    ?>
         <div class="notes_brochures__col">
             <h3>Brochures</h3>
-            <a class="notes_brochures__card" href="<?=$url?>" download="<?=$filename?>">
+            <a class="notes_brochures__card" href="<?=$url?>"
+                download="<?=$filename?>">
                 <div class="overlay"></div>
-                <div class="notes_brochures__title"><?=get_the_title()?></div>
-                <div class="notes_brochures__date"><?=get_the_date()?></div>
+                <div class="notes_brochures__title">
+                    <?=get_the_title()?>
+                </div>
+                <div class="notes_brochures__date">
+                    <?=get_the_date()?>
+                </div>
             </a>
             <?php
-            if ($section == 'corridor') {
-                echo '<div class="link-arrow-inline"><a href="/corridor/knowledge/brochures/">View all Corridor brochures</a></div>';
-            }
-            else {
-                echo '<div class="link-arrow-inline"><a href="/knowledge/brochures/">View all brochures</a></div>';
-            }
-            ?>
+    if ($section == 'corridor') {
+        echo '<div class="link-arrow-inline"><a href="/corridor/knowledge/brochures/">View all Corridor brochures</a></div>';
+    } else {
+        echo '<div class="link-arrow-inline"><a href="/knowledge/brochures/">View all brochures</a></div>';
+    }
+    ?>
         </div>
-            <?php
-        }
-        ?>
+        <?php
+}
+?>
     </div>
 </section>
