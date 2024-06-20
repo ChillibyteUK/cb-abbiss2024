783d77b45c4f9d21a102cb346e10df2639d35657
LamcatUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-06-20 12:01:51 +0100

    Update cb_cta_reviews.php

--- page-templates/blocks/cb_cta_reviews.php
+++ page-templates/blocks/cb_cta_reviews.php
@@ -5,9 +5,9 @@ $l = get_field('link');
     <div class="container-xl">
         <div class="row g-4">
             <div class="col-lg-6 d-flex flex-column">
-                <h3 class="pt-2 border-top">
+                <h2 class="h3 pt-2 border-top">
                     <?=get_field('title')?>
-                </h3>
+                </h2>
                 <a class="cta_reviews__card p-5"
                     href="<?=$l['url']?>">
                     <?=wp_get_attachment_image(get_field('background'), 'full', false, array('class' => 'cta_reviews__background'))?>
@@ -19,8 +19,8 @@ $l = get_field('link');
                 </a>
             </div>
             <div class="col-lg-6 d-flex flex-column">
-                <h3 class="pt-2 border-top">What Clients Say</h3>
-                <?php
+                <h2 class="h3 pt-2 border-top">What Clients Say</h3>
+                    <?php
 $p = new WP_Query(
     array(
         'post_type' => 'testimonials',
@@ -32,36 +32,37 @@ $p = new WP_Query(
 // $size = ($size) ? $size : 30;
 $size = 30;
 ?>
-                <div class="bg-grey-500 h-100 p-4">
-                    <div class="swiper testimonial_slider__slider">
-                        <div class="swiper-wrapper pb-4">
-                            <?php
+                    <div class="bg-grey-500 h-100 p-4">
+                        <div class="swiper testimonial_slider__slider">
+                            <div class="swiper-wrapper pb-4">
+                                <?php
     if ($p->have_posts()) {
         while($p->have_posts()) {
             $p->the_post();
             // cbdump(get_field('logo', get_the_ID()));
             $image_id = get_field('logo', get_the_ID());
-            $image = $image_id ? wp_get_attachment_image($image_id, 'large', false, array('class' => 'testimonial_slider__logo', 'alt' => get_the_title(get_the_ID()))) : null;
+            $image = $image_id ? wp_get_attachment_image($image_id, 'large', false, array('class' => 'testimonial_slider__logo', 'alt' => get_the_title(get_the_ID()), 'loading' => 'lazy' )) : null;
             if ($image ?? null) {
                 ?>
-                            <div class="testimonial_slider__slide swiper-slide py-4">
-                                <?=$image?>
-                                <div class="testimonial_slider__excerpt">
-                                    <?=wp_trim_words(get_field('testimonial', get_the_ID()), $size)?>
+                                <div class="testimonial_slider__slide swiper-slide py-4">
+                                    <?=$image?>
+                                    <div class="testimonial_slider__excerpt">
+                                        <?=wp_trim_words(get_field('testimonial', get_the_ID()), $size)?>
+                                    </div>
                                 </div>
-                            </div>
-                            <?php
+                                <?php
             }
         }
     }
 ?>
+                            </div>
+                            <div class="swiper-pagination swiper-pagination-testimonials"></div>
                         </div>
-                        <div class="swiper-pagination swiper-pagination-testimonials"></div>
+                        <div class="text-center">
+                            <a href="/about-us/testimonials/" class="mx-auto button button-sm button-outline">View
+                                all</a>
+                        </div>
                     </div>
-                    <div class="text-center">
-                        <a href="/about-us/testimonials/" class="mx-auto button button-sm button-outline">View all</a>
-                    </div>
-                </div>
             </div>
         </div>
     </div>
