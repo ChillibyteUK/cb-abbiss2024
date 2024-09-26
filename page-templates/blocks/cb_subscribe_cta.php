f1d71231f18699acf3f962a675e802fa192725fa
ChillibyteUK <83024977+ChillibyteUK@users.noreply.github.com>
2024-09-26 09:44:02 +0100

    subscribe cta

--- page-templates/blocks/cb_subscribe_cta.php
+++ page-templates/blocks/cb_subscribe_cta.php
@@ -1,11 +1,11 @@
-<div class="container-xl">
+<div class="container-xl subscribe_cta">
     <div class="border-top mt-2 py-4">
         <div class="row m-0">
-            <div class="col-lg-8 mb-2 px-0">
+            <div class="col-lg-6 mb-2 px-0">
                 <div class="h3">Subscribe to our newsletter</div>
                 <div>Stay up to the minute on our latest news and insights?</div>
             </div>
-            <div class="col-lg-4 text-lg-right my-auto px-0">
+            <div class="col-lg-6 text-lg-right my-auto px-0">
                 <div class="newsletter">
                     <?=do_shortcode('[gravityform id="12" title="false" description="false" ajax="true"]')?>
                 </div>
